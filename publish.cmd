ed841d6c415e52f87c15d9f01f0e34a24c59d296
Olevedeler <O9l5e567656>
2018-08-22 17:53:41 +0200

    git init

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.ProactiveBot.sln -p:DeployOnBuild=true -p:PublishProfile=brainbot2000-Web-Deploy.pubxml -p:Password=qjqqZabx9hlyMdMvEt79XljGtYWR5YBXLYRdoK1PrBSgtbyvKWRSnWh7rZlc
+
